2e34edd5ccee3c60d6fad2e3c27538656161f22f
sabrine <sabrine.njoumi12@gmail.com>
2018-03-29 12:45:42 +0100

    gitigore

---
+++ src/MainBundle/Admin/FormationAdmin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace MainBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class FormationAdmin extends AbstractAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('titreFormation')
+            ->add('description')
+            ->add('planing')
+            ->add('prix')
+            ->add('duree')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('titreFormation')
+            ->add('description')
+            ->add('planing')
+            ->add('prix')
+            ->add('duree')
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                ),
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('titreFormation')
+            ->add('description')
+            ->add('planing')
+            ->add('prix')
+            ->add('duree')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('titreFormation')
+            ->add('description')
+            ->add('planing')
+            ->add('prix')
+            ->add('duree')
+        ;
+    }
+}

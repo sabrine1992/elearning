b56b1aba255943289459a69f2d3155b7f1eac25d
sabrine <sabrine.njoumi12@gmail.com>
2018-04-08 17:41:39 +0100

    installation

--- src/MainBundle/Controller/ContactController.php
+++ src/MainBundle/Controller/ContactController.php
@@ -1,63 +1,89 @@
 <?php
-namespace myapplication\myBundle\Controller;
+
+
+namespace MainBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+
+use MainBundle\Entity\Contact;
+
 
 class ContactController extends Controller
 {
-    public function contactAction(Request $request)
+
+    /**
+     * @param Request $request
+     */
+    public function contacterAction(Request $request)
     {
-        // Create the form according to the FormType created previously.
-        // And give the proper parameters
-        $form = $this->createForm('MainBundle\Form\ContactType',null,array(
-            // To set the action use $this->generateUrl('route_identifier')
-            'action' => $this->generateUrl('ManBundle_contact'),
-            'method' => 'POST'
-        ));
+        $contact = new Contact;
 
-        if ($request->isMethod('POST')) {
-            // Refill the fields in case the form is not valid.
-            $form->handleRequest($request);
+        # Add form fields
+        $form = $this->createFormBuilder($contact)
+            ->add('name', TextType::class, array('label' => 'name', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('email', TextType::class, array('label' => 'email', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('message', TextareaType::class, array('label' => 'message', 'attr' => array('class' => 'form-control')))
+            ->add('Save', SubmitType::class, array('label' => 'submit', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-top:15px')))
+            ->getForm();
 
-            if($form->isValid()){
-                // Send mail
-                if($this->sendEmail($form->getData())){
+        # Handle form response
+        $form->handleRequest($request);
 
-                    // Everything OK, redirect to wherever you want ! :
 
-                    return $this->redirectToRoute('redirect_to_somewhere_now');
-                }else{
-                    // An error ocurred, handle
-                    var_dump("Errooooor :(");
-                }
+# check if form is submitted
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $name = $form['name']->getData();
+            $email = $form['email']->getData();
+            $message = $form['message']->getData();
+
+            # set form data
+
+            $contact->setName($name);
+            $contact->setEmail($email);
+            $contact->setMessage($message);
+
+            # finally add data in database
+
+            $sn = $this->getDoctrine()->getManager();
+            $sn->persist($contact);
+            $sn->flush();
+
+# check if form is submitted
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $name = $form['name']->getData();
+                $email = $form['email']->getData();
+
+                $message = $form['message']->getData();
+
+                # set form data
+
+                $contact->setName($name);
+                $contact->setEmail($email);
+
+                $contact->setMessage($message);
+
+                # finally add data in database
+
+                $sn = $this->getDoctrine()->getManager();
+                $sn->persist($contact);
+                $sn->flush();
+
+$message = \Swift_Message::newInstance()
+    ->setFrom('sabrine.njoumi12@gmail.com')
+    ->setTo($email)
+    ->setBody($this->renderView('default/sendemail.html.twig',array('name' => $name)),'text/html');
+$this->get('mailer')->send($message);
             }
         }
-
-        return $this->render('MainBundle:Elearning:contact.html.twig', array(
-            'form' => $form->createView()
-        ));
-    }
-
-    private function sendEmail($data){
-        $myappContactMail = 'sabrine.njoumi12@gmail.com';
-        $myappContactPassword = 'sabrine96013201';
-
-        // In this case we'll use the ZOHO mail services.
-        // If your service is another, then read the following article to know which smpt code to use and which port
-        // http://ourcodeworld.com/articles/read/14/swiftmailer-send-mails-from-php-easily-and-effortlessly
-        $transport = \Swift_SmtpTransport::newInstance('smtp.zoho.com', 465,'ssl')
-            ->setUsername($myappContactMail)
-            ->setPassword($myappContactPassword);
-
-        $mailer = \Swift_Mailer::newInstance($transport);
-
-        $message = \Swift_Message::newInstance("Our Code World Contact Form ". $data["subject"])
-            ->setFrom(array($myappContactMail => "Message by ".$data["name"]))
-            ->setTo(array(
-                $myappContactMail => $myappContactMail
-            ))
-            ->setBody($data["message"]."<br>ContactMail :".$data["email"]);
-
-        return $mailer->send($message);
     }
 }

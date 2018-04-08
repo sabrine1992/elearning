4c9d1c8d523b518653b41b2f4373267631450b55
sabrine <sabrine.njoumi12@gmail.com>
2018-04-08 17:43:03 +0100

    gitigore

---
+++ src/MainBundle/Entity/formation.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace MainBundle\Entity;
+
+/**
+ * formation
+ */
+class formation
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nom;
+
+    /**
+     * @var string
+     */
+    private $titre;
+
+    /**
+     * @var string
+     */
+    private $plan;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return formation
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return formation
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set plan
+     *
+     * @param string $plan
+     *
+     * @return formation
+     */
+    public function setPlan($plan)
+    {
+        $this->plan = $plan;
+
+        return $this;
+    }
+
+    /**
+     * Get plan
+     *
+     * @return string
+     */
+    public function getPlan()
+    {
+        return $this->plan;
+    }
+}
+
